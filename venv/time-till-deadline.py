b44b36223936f471cec238299c54394f12ca6594
fernando piñero estrada <fernando.pineroestrada@atos.net>
2022-04-28 14:10:10 +0200

    Añadido ejemplo que lee de un hoja de cálculo y realiza ciertas lógicas con los datos y además escribe un nuevo excel con una columna extra

--- venv/time-till-deadline.py
+++ venv/time-till-deadline.py
@@ -12,7 +12,11 @@ today = datetime.datetime.today() # asigna a today la fecha actual
 
 time_till = (datetime_date - today)
 if time_till.days < 0:
-    print(f"Indica una fecha posterior a hoy {today.date()}")
+    min_fecha = str(datetime.date.today()).split("-")
+    # print(f"min_fecha= {min_fecha}")
+    min_fecha_formateada = min_fecha[2] + "." + min_fecha[1] + "." + min_fecha[0]
+    # print(f"min_fecha_formateada= {min_fecha_formateada}")
+    print(f"Indica una fecha posterior a hoy {min_fecha_formateada}")
 else:
     final_msg = f"Dear User! Time remaining to your goal: {goal} are {time_till.days} days"
     print(final_msg)
